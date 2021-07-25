8e587cdc529d6e710869ad706028b5d299b38e76
Sudi Simba <sudisimbadav@gmail.com>
2021-07-25 10:22:32 +0200

    added Ef Core and Npgsql instance

---
+++ AddresssBook/Data/AddressBookContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AddresssBook.Data
+{
+    public class AddressBookContext : DbContext
+    {
+        public AddressBookContext(DbContextOptions<AddressBookContext> options) : base(options)
+        {
+
+        }
+    }
+}
